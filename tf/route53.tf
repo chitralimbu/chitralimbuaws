c392661d43ac5ff3c74aa7420cd810dd9288338a
chitralimbu <limbu_nikkil@hotmail.co.uk>
2022-08-10 21:26:02 +0100

    Updated gitignore to include tf state files

--- tf/route53.tf
+++ tf/route53.tf
@@ -7,15 +7,17 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "root-a" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
+  ttl     = 300
   type    = "A"
-
+  records = [var.domain_name]
 }
 
 resource "aws_route53_record" "www-a" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
+  ttl     = 300
   type    = "A"
-
+  records = ["www.${var.domain_name}"]
 }
 
 resource "aws_route53_record" "cert_validation" {
