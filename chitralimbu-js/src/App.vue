65c302772ad8b5e8eaf081ac650f6b5e751f5b50
chitralimbu <limbu_nikkil@hotmail.co.uk>
2023-06-11 17:17:16 +0100

    Started SideItem component

--- chitralimbu-js/src/App.vue
+++ chitralimbu-js/src/App.vue
@@ -1,30 +1,90 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!--    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>-->
+    <div class="side-nav">
+      <form class="d-flex">
+        <input
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success" type="submit">
+          <i class="bi bi-search"></i>
+        </button>
+      </form>
+      <div class="accordion">
+        <SideItem
+          :key="index"
+          v-for="(item, index) in sidebar"
+          :item="item"
+        ></SideItem>
+      </div>
+    </div>
+    <div style="margin-left: 25%; padding: 1px 16px; height: 1000px">
+      <router-view />
+    </div>
   </nav>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+body {
+  margin: 0;
+}
+.side-nav {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 25%;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.no-arrow:after {
+  display: none;
 }
 </style>
+<script>
+import SideItem from "@/components/SideItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    SideItem,
+  },
+  data() {
+    return {
+      sidebar: [
+        /*{
+          title: "Title1",
+          list: false,
+          items: [],
+          linkEnabled: true,
+        },*/
+        {
+          title: "Title2",
+          isList: true,
+          subList: [
+            {
+              subTitle: "subTitle-1",
+              link: "page1",
+            },
+            {
+              subTitle: "subtitle-2",
+              link: "page2",
+            },
+          ],
+          linkEnabled: false,
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ chitralimbu-js/src/components/SideItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="accordion-item">
+    <h2 class="accordion-header" :id="item.title">
+      <button
+        class="accordion-button collapsed"
+        type="button"
+        data-bs-toggle="collapse"
+        :data-bs-target="`#${this.title}-panel`"
+        aria-expanded="false"
+        :aria-controls="`${this.title}-panel`"
+      >
+        Accordion Item #2
+      </button>
+    </h2>
+    <div
+      :id="`${this.title}-panel`"
+      class="accordion-collapse collapse"
+      :aria-labelledby="item.title"
+    >
+      <div class="accordion-body">
+        <ul v-if="item.isList === true" class="list-group list-group-flush">
+          <li
+            :key="index"
+            v-for="(listItem, index) in item.subList"
+            class="list-group-item"
+          >
+            {{ listItem.subTitle }}
+          </li>
+          <!--          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>-->
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SideItem",
+  props: {
+    item: Object,
+  },
+};
+</script>
+<style scoped></style>
